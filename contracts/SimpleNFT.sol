a7713a1920880e2dc76e326f5f1e98c6c1de1193
clearence wissar <clearence.wissar@nhvr.gov.au>
2022-02-21 15:31:58 +1000

    working tut

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+
 contract SimpleNFT is ERC721 {
 
     uint256 public tokenCounter;
+    mapping (uint256 => address) tokenIdToMinter;
     
     constructor() ERC721("Simple NFT", "SNFT") {}
 
     function mint() external {
         _safeMint(msg.sender, tokenCounter);
+        tokenIdToMinter[tokenCounter] = msg.sender;
         tokenCounter++;
     }
 
